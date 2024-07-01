6948e3c741a24910ace7d2c7cc5d7cd28a427cbb
Arthur Leal <118284396+arthurL-daSilva@users.noreply.github.com>
2024-06-30 22:57:37 -0300

    Add files via upload

---
+++ Macro.py
@@ -0,0 +1,84 @@
+import pyautogui as bot
+import time, os, sys
+import tkinter as tk
+from tkinter import messagebox
+from pyautogui import ImageNotFoundException
+
+value = 0
+value2 = 0
+image1 = 'E:\\PROJETOS\\Programming Languages\\Python\\Img\\Riotclient.png'
+image2 = 'E:\\PROJETOS\\Programming Languages\\Python\\Img\\Username.png'
+image3 = 'E:\\PROJETOS\\Programming Languages\\Python\\Img\\LOL.png'
+image4 = 'E:\\PROJETOS\\Programming Languages\\Python\\Img\\Username2.png'
+
+def window():
+    root = tk.Tk()
+    root.title("Contas LOL")
+
+
+def CallRiot():
+    try:
+        img1 = bot.locateCenterOnScreen(image1, confidence=0.9)
+
+        if img1 is not None:
+            bot.click(img1.x, img1.y)
+            return 1
+        else:
+            raise ImageNotFoundException
+    except ImageNotFoundException:
+        os.startfile('C:\\Riot Games\\Riot Client\\RiotClientServices.exe')
+        return 0
+
+def Click(number):
+    if number == 0:
+        try:
+            time.sleep(8)
+            img2 = bot.locateCenterOnScreen(image2, confidence=0.8)
+            if img2 is not None:
+                bot.click(img2.x, img2.y)
+                return 1
+            else:
+                raise ImageNotFoundException
+        except ImageNotFoundException:
+            return 0
+    else:
+        try:
+            time.sleep(1.5)
+            img4 = bot.locateCenterOnScreen(image4, confidence=0.7)
+            if img4 is not None:
+                bot.click(img4.x, img4.y)
+                return 1
+            else:
+                raise ImageNotFoundException
+        except ImageNotFoundException:
+            return 0
+
+def typewrite(number):
+    if number == 1:
+        bot.write('account',interval=0.03)
+        bot.press('tab')
+        bot.write('password',interval=0.03)
+        bot.press('enter')
+    else:
+        messagebox.showerror("Error", "Box not found!")
+        sys.exit()
+    
+def ClickOnLol():
+    search = 0
+    while search < 60:
+        try:
+            img3 = bot.locateCenterOnScreen(image3, confidence=0.7)
+            bot.click(img3.x, img3.y)
+            search = 61
+        except:
+            search += 1
+
+    if search == 60:
+        messagebox.showerror("Error", "Box not found!")
+        sys.exit()
+
+
+value2 = CallRiot()
+value = Click(value2)
+typewrite(value)
+ClickOnLol()
