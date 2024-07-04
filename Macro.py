297d3880ac011b1b97d6a4f88b3ea9fd3d1da514
Arthur Leal <118284396+arthurL-daSilva@users.noreply.github.com>
2024-07-03 22:31:04 -0300

    Update Macro.py

--- Macro.py
+++ Macro.py
@@ -6,12 +6,12 @@ from PIL import Image, ImageTk
 from tkinter import messagebox
 from pyautogui import ImageNotFoundException
 
-image1 = 'E:\\PROJETOS\\Programming Languages\\Python\\MacroGaming\\Img\\Riotclient.png'
-image2 = 'E:\\PROJETOS\\Programming Languages\\Python\\MacroGaming\\Img\\Username.png'
-image3 = 'E:\\PROJETOS\\Programming Languages\\Python\\MacroGaming\\Img\\LOL.png'
-image4 = 'E:\\PROJETOS\\Programming Languages\\Python\\MacroGaming\\Img\\Username2.png'
-image5 = 'E:\\PROJETOS\\Programming Languages\\Python\\MacroGaming\\Img\\RiotTop.png'
-image6 = 'E:\\PROJETOS\\Programming Languages\\Python\\MacroGaming\\Img\\logo.png'
+image1 = '\\Img\\Riotclient.png'
+image2 = '\\Img\\Username.png'
+image3 = '\\Img\\LOL.png'
+image4 = '\\Img\\Username2.png'
+image5 = '\\Img\\RiotTop.png'
+image6 = '\\Img\\logo.png'
 data_file = 'account_data.txt'
 
 def CallRiot():
@@ -23,7 +23,7 @@ def CallRiot():
         else:
             raise ImageNotFoundException
     except ImageNotFoundException:
-        os.startfile('C:\\Riot Games\\Riot Client\\RiotClientServices.exe')
+        os.startfile('C:\\Riot Games\\Riot Client\\RiotClientServices.exe') # <-- Your Riot Client Path Here
         return 0
 
 def Click(number):
@@ -59,7 +59,7 @@ def typewrite_account(username, password, number):
         bot.write(password, interval=0.03)
         bot.press('enter')
     else:
-        messagebox.showerror("Erro", "Caixa de texto não encontrada!")
+        messagebox.showerror("Error", "Box not found!")
         sys.exit()
 
 def ClickOnLol():
@@ -76,26 +76,26 @@ def ClickOnLol():
             search += 1
 
     if search == 60:
-        messagebox.showerror("Erro", "Imagem não encontrada!")
+        messagebox.showerror("Error", "Image not found!")
         sys.exit()
 
 def load_account_data():
     if not os.path.exists(data_file):
-        messagebox.showwarning("Aviso", "Arquivo de dados não encontrado. Adicione alguma conta para cria-lo.")
+        messagebox.showwarning("Warning", "No file found, add at least one account to create it.")
         return
 
     with open(data_file, 'r') as file:
         lines = file.readlines()
         if len(lines) < 1:
-            messagebox.showwarning("Aviso", "Nenhuma conta encontrada.")
+            messagebox.showwarning("Warning", "No accounts found!")
             return
 
         if len(lines) >= 1:
-            entry_user1.insert(0, lines[0].strip()) #0  -> começando na posição 0
+            entry_user1.insert(0, lines[0].strip()) 
         if len(lines) >= 2:
-            entry_pass1.insert(0, lines[1].strip()) #lines[1] -> seria o número da linha contida no arquivo
+            entry_pass1.insert(0, lines[1].strip()) 
         if len(lines) >= 3:
-            entry_user2.insert(0, lines[2].strip()) #.strip() -> Remove espaços em branco
+            entry_user2.insert(0, lines[2].strip()) 
         if len(lines) >= 4:
             entry_pass2.insert(0, lines[3].strip())
         if len(lines) >= 5:
@@ -109,7 +109,7 @@ def load_account_data():
 
 def save_account_data():
     with open(data_file, 'w') as file:
-        file.write(entry_user1.get() + '\n') #entry_user.get() -> escreve o conteúdo do campo de entrada no arquivo, seguido por um caracter de nova linha '\n'
+        file.write(entry_user1.get() + '\n')
         file.write(entry_pass1.get() + '\n')
         file.write(entry_user2.get() + '\n')
         file.write(entry_pass2.get() + '\n')
@@ -119,7 +119,7 @@ def save_account_data():
         file.write(entry_pass4.get() + '\n') 
 
 root = tk.Tk()
-root.title("Minhas contas - (League of Legends)")
+root.title("My accounts - (League of Legends)")
 root.geometry("450x300")
 image = Image.open(image6)
 image = image.resize((64, 64), Image.Resampling.LANCZOS)
@@ -129,52 +129,52 @@ root.resizable(width=False, height=False)
 label_name = Label(root, text='Contas:', font=('Arial 10 bold'))
 label_name.place(x=10, y=10)
 
-label_user1 = Label(root, text="Conta 1:")
+label_user1 = Label(root, text="Account 1:")
 label_user1.place(x=10, y=50)
 entry_user1 = Entry(root)
 entry_user1.place(x=100, y=50)
 
-label_pass1 = Label(root, text="Senha 1:")
+label_pass1 = Label(root, text="Password 1:")
 label_pass1.place(x=10, y=80)
 entry_pass1 = Entry(root, show='*')
 entry_pass1.place(x=100, y=80)
 
-label_user2 = Label(root, text="Conta 2:")
+label_user2 = Label(root, text="Account 2:")
 label_user2.place(x=10, y=110)
 entry_user2 = Entry(root)
 entry_user2.place(x=100, y=110)
 
-label_pass2 = Label(root, text="Senha 2:")
+label_pass2 = Label(root, text="Password 2:")
 label_pass2.place(x=10, y=140)
 entry_pass2 = Entry(root, show='*')
 entry_pass2.place(x=100, y=140)
 
-label_user3 = Label(root, text="Conta 3:")
+label_user3 = Label(root, text="Account 3:")
 label_user3.place(x=10, y=170)
 entry_user3 = Entry(root)
 entry_user3.place(x=100, y=170)
 
-label_pass3 = Label(root, text="Senha 3:")
+label_pass3 = Label(root, text="Password 3:")
 label_pass3.place(x=10, y=200)
 entry_pass3 = Entry(root, show='*')
 entry_pass3.place(x=100, y=200)
 
-label_user4 = Label(root, text="Conta 4:")
+label_user4 = Label(root, text="Account 4:")
 label_user4.place(x=10, y=230)
 entry_user4 = Entry(root)
 entry_user4.place(x=100, y=230)
 
-label_pass4 = Label(root, text="Senha 4:")
+label_pass4 = Label(root, text="Password 4:")
 label_pass4.place(x=10, y=260)
 entry_pass4 = Entry(root, show='*')
 entry_pass4.place(x=100, y=260)
 
 def acc1():
     if not entry_user1.get() or not entry_pass1.get():
-        messagebox.showwarning("Aviso", "Usuário ou senha da conta 1 não está registrado.")
+        messagebox.showwarning("Warning", "Username or password of account 1 is not registered!")
         return
 
-    label_name.config(text="Entrando na Conta 1")
+    label_name.config(text="Entering account 1...")
     value2 = CallRiot()
     value = Click(value2)
     username = entry_user1.get()
@@ -185,10 +185,10 @@ def acc1():
 
 def acc2():
     if not entry_user2.get() or not entry_pass2.get():
-        messagebox.showwarning("Aviso", "Usuário ou senha da conta 2 não está registrado.")
+        messagebox.showwarning("Warning", "Username or password of account 2 is not registered!")
         return
 
-    label_name.config(text="Entrando na Conta 2")
+    label_name.config(text="Entering account 2...")
     value2 = CallRiot()
     value = Click(value2)
     username = entry_user2.get()
@@ -199,10 +199,10 @@ def acc2():
 
 def acc3():
     if not entry_user3.get() or not entry_pass3.get():
-        messagebox.showwarning("Aviso", "Usuário ou senha da conta 3 não está registrado.")
+        messagebox.showwarning("Warning", "Username or password of account 3 is not registered!")
         return
 
-    label_name.config(text="Entrando na Conta 3")
+    label_name.config(text="Entering account 3...")
     value2 = CallRiot()
     value = Click(value2)
     username = entry_user3.get()
@@ -213,10 +213,10 @@ def acc3():
 
 def acc4():
     if not entry_user4.get() or not entry_pass4.get():
-        messagebox.showwarning("Aviso", "Usuário ou senha da conta 4 não está registrado.")
+        messagebox.showwarning("Warning", "Username or password of account 4 is not registered!")
         return
 
-    label_name.config(text="Entrando na Conta 4")
+    label_name.config(text="Entering account 4...")
     value2 = CallRiot()
     value = Click(value2)
     username = entry_user4.get()
@@ -225,16 +225,16 @@ def acc4():
     save_account_data()
     ClickOnLol()
 
-button1 = tk.Button(root, text="Entrar na Conta 1", command=acc1, relief='groove')
+button1 = tk.Button(root, text="Enter account 1", command=acc1, relief='groove')
 button1.place(x=300, y=50)
 
-button2 = tk.Button(root, text="Entrar na Conta 2", command=acc2, relief='groove')
+button2 = tk.Button(root, text="Enter account 2", command=acc2, relief='groove')
 button2.place(x=300, y=110)
 
-button3 = tk.Button(root, text="Entrar na Conta 3", command=acc3, relief='groove')
+button3 = tk.Button(root, text="Enter account 3", command=acc3, relief='groove')
 button3.place(x=300, y=170)
 
-button4 = tk.Button(root, text="Entrar na Conta 4", command=acc4, relief='groove')
+button4 = tk.Button(root, text="Enter account 4", command=acc4, relief='groove')
 button4.place(x=300, y=230)
 
 load_account_data()
