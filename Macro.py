063e7bad22f3bb51b00405c0ef4940c23a694add
Arthur Leal <118284396+arthurL-daSilva@users.noreply.github.com>
2024-07-07 21:26:46 -0300

    Add files via upload

--- Macro.py
+++ Macro.py
@@ -3,6 +3,7 @@ import time, os, sys
 import threading
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
 from PIL import Image, ImageTk
 from tkinter import messagebox
 from pyautogui import ImageNotFoundException
@@ -13,7 +14,9 @@ image3 = '\\Img\\LOL.png'
 image4 = '\\Img\\Username2.png'
 image5 = '\\Img\\RiotTop.png'
 image6 = '\\Img\\logo.png'
+image7 = '\\Img\\League_open.png'
 data_file = 'account_data.txt'
+data_path = 'Riot_Client_path.txt'
 
 def CallRiot():
     try:
@@ -24,7 +27,7 @@ def CallRiot():
         else:
             raise ImageNotFoundException
     except ImageNotFoundException:
-        os.startfile('C:\\Riot Games\\Riot Client\\RiotClientServices.exe') # <--- Put your path in here
+        os.startfile(application_path.get())
         return 0
 
 def Click(number):
@@ -55,9 +58,9 @@ def Click(number):
 
 def typewrite_account(username, password, number):
     if number == 1:
-        bot.write(username, interval=0.03)
+        bot.write(username, interval=0.02)
         bot.press('tab')
-        bot.write(password, interval=0.03)
+        bot.write(password, interval=0.02)
         bot.press('enter')
     else:
         messagebox.showerror("Error", "Text box not found!")
@@ -89,7 +92,7 @@ def load_account_data():
         lines = file.readlines()
 
         if len(lines) >= 1:
-            entry_user1.insert(0, lines[0].strip()) 
+            entry_user1.insert(0, lines[0].strip())
         if len(lines) >= 2:
             entry_pass1.insert(0, lines[1].strip())
         if len(lines) >= 3:
@@ -114,65 +117,107 @@ def save_account_data():
         file.write(entry_user3.get() + '\n')
         file.write(entry_pass3.get() + '\n')
         file.write(entry_user4.get() + '\n')
-        file.write(entry_pass4.get() + '\n') 
+        file.write(entry_pass4.get() + '\n')
+
+def League_Open():
+    try:
+        img7 = bot.locateCenterOnScreen(image7, confidence=0.7)
+        if img7 is not None:
+            messagebox.showwarning('Warning','Your game is already open!')
+            return 0
+        else:
+            raise ImageNotFoundException
+    except ImageNotFoundException:
+        return 1
 
 root = tk.Tk()
 root.title("My accounts - (League of Legends)")
-root.geometry("450x300")
+root.geometry("650x370")
 image = Image.open(image6)
 image = image.resize((64, 64), Image.Resampling.LANCZOS)
 icon = ImageTk.PhotoImage(image)
 root.iconphoto(False, icon)
-root.resizable(width=False, height=False)
+application_path = StringVar()
 label_name = Label(root, text='Accounts:', font=('Arial 10 bold'))
 label_name.place(x=10, y=10)
 
-label_user1 = Label(root, text="Accounts 1:")
-label_user1.place(x=10, y=50)
+label_footer_main = Label(root, text='---------------------------------------------------------------------------------------------------------------------------------------------------------------', font='Arial 10')
+label_footer_main.place(x=5, y=30)
+
+label_user1 = Label(root, text="Account 1:")
+label_user1.place(x=10, y=60)
 entry_user1 = Entry(root)
-entry_user1.place(x=100, y=50)
+entry_user1.place(x=100, y=60)
 
 label_pass1 = Label(root, text="Password:")
-label_pass1.place(x=10, y=80)
+label_pass1.place(x=10, y=85)
 entry_pass1 = Entry(root, show='*')
-entry_pass1.place(x=100, y=80)
+entry_pass1.place(x=100, y=85)
 
-label_user2 = Label(root, text="Accounts 2:")
-label_user2.place(x=10, y=110)
+label_user2 = Label(root, text="Account 2:")
+label_user2.place(x=10, y=140)
 entry_user2 = Entry(root)
-entry_user2.place(x=100, y=110)
+entry_user2.place(x=100, y=140)
 
 label_pass2 = Label(root, text="Password:")
-label_pass2.place(x=10, y=140)
+label_pass2.place(x=10, y=165)
 entry_pass2 = Entry(root, show='*')
-entry_pass2.place(x=100, y=140)
+entry_pass2.place(x=100, y=165)
 
-label_user3 = Label(root, text="Accounts 3:")
-label_user3.place(x=10, y=170)
+label_user3 = Label(root, text="Account 3:")
+label_user3.place(x=10, y=220)
 entry_user3 = Entry(root)
-entry_user3.place(x=100, y=170)
+entry_user3.place(x=100, y=220)
 
 label_pass3 = Label(root, text="Password:")
-label_pass3.place(x=10, y=200)
+label_pass3.place(x=10, y=245)
 entry_pass3 = Entry(root, show='*')
-entry_pass3.place(x=100, y=200)
+entry_pass3.place(x=100, y=245)
 
-label_user4 = Label(root, text="Accounts 4:")
-label_user4.place(x=10, y=230)
+label_user4 = Label(root, text="Account 4:")
+label_user4.place(x=10, y=300)
 entry_user4 = Entry(root)
-entry_user4.place(x=100, y=230)
+entry_user4.place(x=100, y=300)
 
 label_pass4 = Label(root, text="Password:")
-label_pass4.place(x=10, y=260)
+label_pass4.place(x=10, y=325)
 entry_pass4 = Entry(root, show='*')
-entry_pass4.place(x=100, y=260)
+entry_pass4.place(x=100, y=325)
+
+label_path = Label(root, text="Riot Client not selected.", font='Arial 8 bold')
+label_path.place(x=480, y=13)
 
 def execute_account(username, password):
-    value2 = CallRiot()
-    value = Click(value2)
-    typewrite_account(username, password, value)
-    save_account_data()
-    ClickOnLol()
+    
+    with open(data_path, 'r') as file:
+        lines = file.readlines()
+        if len(lines) >= 1:
+            value3 = League_Open()
+            if value3 == 1:
+                value2 = CallRiot()
+                value = Click(value2)
+                typewrite_account(username, password, value)
+                save_account_data()
+                ClickOnLol()
+        else:
+            messagebox.showerror('Select the Riot Client.', 'Riot Client not selected')
+
+def select_RiotClient():
+    file_path = filedialog.askopenfilename(title="Select the Riot Client", filetypes=[("executables", "*.exe")])
+    if file_path:
+        application_path.set(file_path)
+        label_path.config(text="Riot Client ready!")
+        with open(data_path, 'w') as file:
+            file.write(file_path)
+
+def load_RiotClient_path():
+    if os.path.exists(data_path):
+        with open(data_path, 'r') as file:
+            file_path = file.read().strip()
+            if file_path:
+                application_path.set(file_path)
+                label_path.config(text="Riot Client ready!")
+
 
 def acc1():
     if not entry_user1.get() or not entry_pass1.get():
@@ -206,18 +251,22 @@ def acc4():
     label_name.config(text="Entrando na "+ entry_user4.get())
     threading.Thread(target=execute_account, args=(entry_user4.get(), entry_pass4.get())).start()
 
-button1 = tk.Button(root, text="Enter in Acc 1", command=acc1, relief='groove')
-button1.place(x=300, y=50)
+button1 = tk.Button(root, text="Enter in acc 1", command=acc1, relief='groove')
+button1.place(x=300, y=70)
 
-button2 = tk.Button(root, text="Enter in Acc 2", command=acc2, relief='groove')
-button2.place(x=300, y=110)
+button2 = tk.Button(root, text="Enter in acc 2", command=acc2, relief='groove')
+button2.place(x=300, y=152)
 
-button3 = tk.Button(root, text="Enter in Acc 3", command=acc3, relief='groove')
-button3.place(x=300, y=170)
+button3 = tk.Button(root, text="Enter in acc 3", command=acc3, relief='groove')
+button3.place(x=300, y=232)
 
-button4 = tk.Button(root, text="Enter in Acc 4", command=acc4, relief='groove')
-button4.place(x=300, y=230)
+button4 = tk.Button(root, text="Enter in acc 4", command=acc4, relief='groove')
+button4.place(x=300, y=312)
+
+button_path = tk.Button(root, text="Select RiotClient", command=select_RiotClient, relief='groove')
+button_path.place(x=475, y=70)
 
 load_account_data()
+load_RiotClient_path()
 
 root.mainloop()
